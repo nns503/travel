399d6f36cd7aa991b5e6f2e1790d9c387091b35b
nns503 <73779581+nns503@users.noreply.github.com>
2024-09-04 15:20:46 +0900

    초기 세팅

---
+++ src/test/java/backend/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
